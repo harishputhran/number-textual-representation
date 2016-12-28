971fff065e9090febdbcffce6bb640b098fd60bf
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 21:38:16 +0530

    GREEN: Case - Return Text for Input Number 1.

--- src/main/java/com/clean/code/NumberTextSpeller.java
+++ src/main/java/com/clean/code/NumberTextSpeller.java
@@ -2,7 +2,7 @@ package com.clean.code;
 
 public class NumberTextSpeller {
 
-	public String getText(int inputNumber) {		
-		return "ZERO";
+	public String getText(int inputNumber) {
+		return inputNumber == 1 ? "ONE" : "ZERO";		
 	}
 }
