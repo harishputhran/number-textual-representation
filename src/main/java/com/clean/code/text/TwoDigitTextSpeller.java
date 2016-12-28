c8e40cce0bb517e034479a5ffe9174d2644055f6
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 23:55:43 +0530

    REFACTOR-StringBuilder to concatenate Texts. Added text for 30 till 90.

--- src/main/java/com/clean/code/text/TwoDigitTextSpeller.java
+++ src/main/java/com/clean/code/text/TwoDigitTextSpeller.java
@@ -8,7 +8,9 @@ public class TwoDigitTextSpeller extends SingleDigitTextSpeller{
 	
 	private final int INDEX_VALUE_10 = 10;
 	private final String BLANK_SPACE_LITERAL = " ";
-	private final List<String> tensText = asList("", "", "TWENTY");
+	
+	// First 2 entries are left blank to facilitate easy fetch for numbers from 20.
+	private final List<String> tensText = asList("", "", "TWENTY", "THIRTY", "FORTY", "FIFTY", "SIXTY", "SEVENTY", "EIGHTY", "NINETY");
 	private final List<String> tenTillTwentyText = asList("TEN", "ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN");
 	
 	public String getText(int number){
@@ -16,21 +18,27 @@ public class TwoDigitTextSpeller extends SingleDigitTextSpeller{
 										: getTextForNumberGreaterThan20(number);
 	}
 
-	public boolean isBetween10and20(int number) {
+	protected boolean isBetween10and20(int number) {
 		return number >= 10 && number < 20;
 	}
 	
-	private String getTextForNumberGreaterThan20(int number) {
+	protected String getTextForNumberGreaterThan20(int number) {
 		int remainder = number % INDEX_VALUE_10;
 		int multiplerOfTen = number / INDEX_VALUE_10;
 		
 		return isMultipleOfTen(remainder) ? tensText.get(multiplerOfTen) 
-				                          : tensText.get(multiplerOfTen) 
-				                            + BLANK_SPACE_LITERAL 
-				                            + super.getText(remainder);
+				                          : generateText(remainder, multiplerOfTen);
 	}
 
-	public boolean isMultipleOfTen(int remainder) {
+	protected String generateText(int remainder, int multiplerOfTen) {
+		StringBuilder builder = new StringBuilder();
+		builder.append(tensText.get(multiplerOfTen));
+		builder.append(BLANK_SPACE_LITERAL);
+		builder.append(super.getText(remainder));
+		return builder.toString();
+	}
+
+	protected boolean isMultipleOfTen(int remainder) {
 		return remainder == 0;
 	}
 }
