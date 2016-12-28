36fd3179209c52b74421aaaf87c9997f9884e471
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-29 02:15:42 +0530

    REFACTOR - Code format and removed unnecessary methods.

--- src/main/java/com/clean/code/text/TwoDigitTextSpeller.java
+++ src/main/java/com/clean/code/text/TwoDigitTextSpeller.java
@@ -4,59 +4,59 @@ import static java.util.Arrays.asList;
 
 import java.util.List;
 
-public class TwoDigitTextSpeller extends SingleDigitTextSpeller{
-	
+public class TwoDigitTextSpeller extends SingleDigitTextSpeller {
+
+	private static final String EMPTY_LITERAL = "";
 	private final int INDEX_VALUE = 10;
 	protected final String BLANK_SPACE_LITERAL = " ";
-	
-	// First 2 entries are left blank to facilitate easy fetch for numbers from 20.
-	private final List<String> tensText = asList("", "", "TWENTY", "THIRTY", "FORTY", "FIFTY", "SIXTY", "SEVENTY", "EIGHTY", "NINETY");
-	private final List<String> tenTillTwentyText = asList("TEN", "ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN");
 
+	// First 2 entries are left blank to facilitate easy fetch for numbers from 20.
+	private final List<String> tensText = asList(EMPTY_LITERAL, EMPTY_LITERAL, "TWENTY", "THIRTY", "FORTY", "FIFTY", 
+												 "SIXTY", "SEVENTY", "EIGHTY", "NINETY");
 	
-	public String getText(int number){
-		return isBetween10and20(number) ? tenTillTwentyText.get(number - 10) 
+	private final List<String> tenTillTwentyText = asList("TEN", "ELEVEN", "TWELVE", "THIRTEEN", 
+														  "FOURTEEN", "FIFTEEN", "SIXTEEN",
+														  "SEVENTEEN", "EIGHTEEN", "NINETEEN");
+
+	public String getText(int number) {
+		return isBetween10and20(number) ? tenTillTwentyText.get(number - 10)
 										: getTextForNumberGreaterThan20(number);
 	}
-	
+
 	protected String getTextForNumberGreaterThan20(int number) {
 		int remainingDigits = getRemainingDigits(number, INDEX_VALUE);
 		int digitAtTenthDecimalIndex = getDigitAtHighestDecimalIndex(number, INDEX_VALUE);
-		
-		return isMultiple(remainingDigits) ? tensText.get(digitAtTenthDecimalIndex) 
-				                           : generateText(tensText.get(digitAtTenthDecimalIndex), 
-				                        		          super.getText(remainingDigits));
+
+		return remainingDigits == 0 ? tensText.get(digitAtTenthDecimalIndex) 
+									: generateText(tensText.get(digitAtTenthDecimalIndex),
+																super.getText(remainingDigits));
 	}
-	
+
 	protected boolean isBetween10and20(int number) {
 		return number >= 10 && number < 20;
 	}
 
-	protected int getDigitAtHighestDecimalIndex(int number, int decimalIndexValue) {
-		return number/decimalIndexValue;
+	protected int getDigitAtHighestDecimalIndex(int number,
+			int decimalIndexValue) {
+		return number / decimalIndexValue;
 	}
 
 	protected int getRemainingDigits(int number, int decimalIndexValue) {
 		return number % decimalIndexValue;
 	}
 
-	protected boolean isMultiple(int remainder) {
-		return remainder == 0;
-	}
-	
 	protected String generateText(String... texts) {
 		StringBuilder builder = new StringBuilder();
-		for(String text : texts){
-			if(isNotBlank(text)){
+		for (String text : texts) {
+			if (isNotBlank(text)) {
 				builder.append(text);
 				builder.append(BLANK_SPACE_LITERAL);
-			}			
-		}		
+			}
+		}
 		return builder.toString().trim();
 	}
 
-	private boolean isNotBlank(String text) {		
+	private boolean isNotBlank(String text) {
 		return text.trim().length() > 0;
 	}
 }
-	
