3ad4d4392bde499cbbfc3af1f21bee083f04638c
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 22:02:29 +0530

    REFACTOR - Create Map as instance variable and initialize in constructor.

--- src/main/java/com/clean/code/NumberTextSpeller.java
+++ src/main/java/com/clean/code/NumberTextSpeller.java
@@ -4,12 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class NumberTextSpeller {
-
-	public String getText(int inputNumber) {
-		Map<Integer, String> numberToText = new HashMap<>();
+	
+	private Map<Integer, String> numberToText;
+	
+	public NumberTextSpeller(){
+		numberToText = new HashMap<>();
 		numberToText.put(0, "ZERO");
 		numberToText.put(1, "ONE");
 		numberToText.put(9, "NINE");
+	}
+
+	public String getText(int inputNumber) {
 		return numberToText.get(inputNumber);		
 	}
 }
