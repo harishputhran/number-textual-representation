95cb3d31d28e10f5868e2850ce3b8f60854b74fa
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 21:59:21 +0530

    GREEN: Case - Return Text for Input Number 9.

--- src/main/java/com/clean/code/NumberTextSpeller.java
+++ src/main/java/com/clean/code/NumberTextSpeller.java
@@ -1,8 +1,15 @@
 package com.clean.code;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberTextSpeller {
 
 	public String getText(int inputNumber) {
-		return inputNumber == 1 ? "ONE" : "ZERO";		
+		Map<Integer, String> numberToText = new HashMap<>();
+		numberToText.put(0, "ZERO");
+		numberToText.put(1, "ONE");
+		numberToText.put(9, "NINE");
+		return numberToText.get(inputNumber);		
 	}
 }
